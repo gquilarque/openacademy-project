485a4a8487e5f4998ce10e2549090f4a1a873482
Gabriela Quilarque <gabriela@vauxoo.com>
2016-12-15 13:24:53 -0400

    [IMP] Added _inherit to view so model.

---
+++ openacademy/model/partner.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    instructor_id = fields.Boolean("Instructor", default=False)
+    session_ids = fields.Many2many("openacademy.session",string="Attended Sessions", readonly=True)
