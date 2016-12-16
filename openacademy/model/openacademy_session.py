f70f3db829c4bfd5293ad835467f3322fca472bb
Gabriela Quilarque <gabriela@vauxoo.com>
2016-12-15 20:13:05 -0400

    [IMP] openacademy: Added domain in field

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -8,5 +8,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    user_id = fields.Many2one('res.partner', string='Instructor')
+    user_id = fields.Many2one('res.partner', string='Instructor',
+                                             domain=['|',('instructor_id', '=', True),
+                                                         ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course', string='Course', required=True) 
