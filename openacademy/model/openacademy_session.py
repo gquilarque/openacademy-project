827ff63f1db439f5d123976bac662cecfbecd0a3
Gabriela Quilarque <gabriela@vauxoo.com>
2016-12-16 12:24:37 -0400

    [ADD] openacademy: Added field active. [ADD] Added field date with default value.

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -5,7 +5,7 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     user_id = fields.Many2one('res.partner', string='Instructor',
@@ -14,6 +14,7 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', string='Course', required=True) 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
