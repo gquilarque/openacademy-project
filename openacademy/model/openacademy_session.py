6bd8ea6d2fad2d1d192db6c62f215decb63a18ef
Gabriela Quilarque <gabriela@vauxoo.com>
2016-12-20 10:02:35 -0400

    [IMP] Day3 CTP322 01:57:00 Gantt View.

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -18,7 +18,8 @@ class Session(models.Model):
     active = fields.Boolean(default=True)
     end_date = fields.Date(string="End Date", store=True,
         compute='_get_end_date', inverse='_set_end_date')
-
+    hours = fields.Float(string="Duration in hours",
+                         compute='_get_hours', inverse='_set_hours')
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -75,3 +76,11 @@ class Session(models.Model):
             end_date = fields.Datetime.from_string(r.end_date)
             r.duration = (end_date - start_date).days + 1
 
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
