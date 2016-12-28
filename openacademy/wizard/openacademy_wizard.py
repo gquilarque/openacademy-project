78d23ffe30289a2177ca6ffa6d36661b2d23aa18
Gabriela Quilarque <gabriela@vauxoo.com>
2016-12-28 10:03:24 -0400

    [REF] Day4 CTPT42 02:25:00 Added complex wizard.

--- openacademy/wizard/openacademy_wizard.py
+++ openacademy/wizard/openacademy_wizard.py
@@ -6,13 +6,14 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
     def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
 
-    session_id = fields.Many2one('openacademy.session',
+    session_ids = fields.Many2many('openacademy.session',
         string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     @api.multi
     def subscribe(self):
-        self.session_id.attendee_ids |= self.attendee_ids
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
         return {}
