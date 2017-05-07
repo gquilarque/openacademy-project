2140d11210f85054a8c63d83d49aaa5f56761951
Gabriela <gabrielaquilarque97@gmail.com>
2017-05-07 00:12:09 +0000

    [REF] Added 2 new UnitTest for openacademy.courses
    * Test Name: test_10_two_courses_same_name
    * Test Description: Test to create two course with same name.To raise
    constraint of unique name.
    
    * Test Name: test_15_duplicate_course
    * Test Description: Test to duplicate course and check that this work fine.

--- openacademy/tests/test_openacademy_course.py
+++ openacademy/tests/test_openacademy_course.py
@@ -29,7 +29,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
     # Test Method start with 'def test_*(self)'
     # Mute Error openerp.sql_db to avoid it in log
     @mute_logger('openerp.sql_db') 
-    def test_01_same_name_description(self):
+    def test_05_same_name_description(self):
         '''
         Test create a course with same name and description
         to test constraint of name and description are different.
@@ -41,4 +41,32 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
             'check constraint "openacademy_course_name_description_check"'):
             # Create a course with same name and description to raise error
             self.create_course('test','test',None)
-        
+
+    @mute_logger('openerp.sql_db')
+    def test_10_two_courses_same_name(self):
+        '''
+        Test to create two course with same name.
+        To raise constraint of unique name.
+        '''
+        self.create_course('test_name','description_1',None)
+        with self.assertRaisesRegexp(
+            IntegrityError,
+            'duplicate key value violates unique constraint '
+            '"openacademy_course_name_unique"'
+            ):
+            self.create_course('test_name','description_2',None)
+
+    #Test a ideal functionality
+    def test_15_duplicate_course(self):
+        '''
+        Test to duplicate course and check that this work fine.
+        '''
+        course = self.env.ref('openacademy.course3')
+        course.copy()
+
+
+
+
+
+
+
